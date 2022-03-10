4a855052cef48ba149964b8f20ff630d1fd954e0
JesusSanchez23 <jesus8347@gmail.com>
2022-03-10 16:40:59 -0600

    routers

---
+++ controllers/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+namespace Controllers;
+
+
+
+class LoginController{
+
+    public static function login(){
+        echo "desde el login";
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+    }
+
+    public static function logout(){
+
+    }
+
+
+    public static function crear(){
+
+        echo "desde crear";
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+    }
+
+    public static function olvide(){
+
+        echo "desde olvide";
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+    }
+
+    public static function reestablecer(){
+
+        echo "desde reestablecer";
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+    }
+
+    public static function mensaje(){
+
+
+        
+    }
+
+    public static function confirmar(){
+
+    }
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3208e339ee61e96ff90ee267f24ce79164d3b81e',
+        'name' => 'juandelatorre/up-task_mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'juandelatorre/up-task_mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3208e339ee61e96ff90ee267f24ce79164d3b81e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
